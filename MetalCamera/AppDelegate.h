6f57fe0421498cb621e1322dc8b29e6371202078
Max <mczonk@gmail.com>
2014-08-30 20:37:46 +0200

    Initial Commit

---
+++ MetalCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MetalCamera
+//
+//  Created by Maximilian Christ on 30/08/14.
+//  Copyright (c) 2014 McZonk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MetalCamera/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  MetalCamera
+//
+//  Created by Maximilian Christ on 30/08/14.
+//  Copyright (c) 2014 McZonk. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GameViewController : UIViewController
+
+@end
+
